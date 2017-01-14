5f41d1ddf5882e1683283f81caf9bfc73474a61e
johnatta <johnatta.costa@outlook.com>
2017-01-13 23:14:28 -0200

    primeiro comentario

---
+++ src/Testando.java
@@ -0,0 +1,10 @@
+
+public class Testando {
+	
+	private void syso() {
+		// TODO Auto-generated method stub
+		System.out.println("tesssstando");
+
+	}
+
+}
---
+++ src/TesteConfiguracaoJUnit.java
@@ -0,0 +1,11 @@
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TesteConfiguracaoJUnit {
+
+	@Test
+	public void testeConfiguracao(){
+		Assert.assertEquals(2, (1+1), 0);
+	}
+}
